7d85fb6a50f1a725529a164041b55207f85d7434
SushilMishra0712 <53597543+SushilMishra0712@users.noreply.github.com>
2019-09-23 15:58:17 +0530

    Add files via upload

---
+++ program4.php
@@ -0,0 +1,13 @@
+<?php
+echo "PHP String Functions:"."\n"."\n";
+echo "a)Get the length of the String"."\n";
+echo strlen("Hello World!")."\n";
+echo "b)Count the number of words in a string"."\n";
+echo str_word_count("Hello World!")."\n";
+echo "c)Reverse a String"."\n";
+echo strrev("Reverse String")."\n";
+echo "d)Search for a Specific Text Within a String"."\n";
+echo strpos("How are you?","you")."\n";
+echo "e)Replace Text within a String:"."\n";
+echo str_replace("world","Dolly","Hello world!")."\n";
+?>
